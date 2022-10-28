f2914944f6a396a694c146d96322ed115bee1c4b
lolster45 <oscarneutron3@gmail.com>
2022-10-27 23:38:43 -0500

    updated

--- script.js
+++ script.js
@@ -1,133 +1,159 @@
-const numberContainer = document.querySelector("#number-container");
-const operatorsCont = document.querySelector("#operators-cont");
-const display = document.querySelector("#display");
-const equalsBtn = document.getElementById("equal");
-const oprtsNew = ["0","1","2","3","4","5","6","7","8","9"];
-const history = document.querySelector(".history");
-const histDisplay = document.getElementById("hist-display");
-const historyBtn = document.getElementById("historyBtn")
+const operators = document.querySelectorAll(".operator");
+const specialBtns = document.querySelectorAll(".special");
+const integers = document.querySelectorAll(".integer")
+const display = document.getElementById("display");
+const oprts = ["+","-","*","/"]
 
-//----------------------------------------------------
-let ref = "";
-numberContainer.addEventListener("click", (e) => {
-  const displayedNumber = display.textContent;
-  const prevValue = display.textContent;
-
-  if (display.textContent.length > 9) {
-    alert("Maximum characters reached!")
-    return
-  };
-
-  if (display.textContent.length > 6) { 
-    display.style.fontSize = "38px" 
-    display.innerHTML = `${display.textContent} Hello` 
+const handleIntegers = (e) => {
+  if (display.textContent === "0") {
+      display.textContent = e.target.textContent;
   }
-
-  if (!e.target.closest("button")) return //fixes glitch where you press in between and all the numbers pop-up.
-
-  if (displayedNumber === '0') { // Replaces the 0 in display with clicked number.
-    display.textContent = e.target.textContent
-  } 
   else {
-    display.textContent = displayedNumber + e.target.textContent
+      display.textContent += e.target.textContent;
   }
-  
-  // Adds decimal abilities.
-  if (displayedNumber === "0" && e.target.textContent === ".") {
-     return display.textContent = displayedNumber + ".";
-  }
-  if ( //Adds 0 before the decimal place when using decimals.
-    e.target.textContent === "." && 
-    !oprtsNew.includes(prevValue[display.textContent.length - 2])
-    ) {
-    return display.textContent = `${displayedNumber}0${e.target.textContent}`;
-  }
+} 
 
-  if (String(ref) === displayedNumber) {
-    console.log("active")
-    if (displayedNumber === String(ref)) {
-      display.textContent = e.target.textContent
-    } 
+const addOprts = (e) => {
+    const previousVal = display.textContent.split(" ")[display.textContent - 1]
+    const oprts = ["+","-","*","%"]
+    const displayedNumber = display.textContent;
+
+    if (display.textContent === "0") { //Makes it not possible to have operator as first value
+        return; 
+    }  
+    const vEquation = displayedNumber.split(" ")
+
+    if (!oprts.includes(previousVal)) { 
+        display.textContent = `${vEquation[0]} ${e.target.textContent} `;
+    }
     else {
-      display.textContent = displayedNumber + e.target.textContent
+        const result = display.textContent.split(" ");
+        result.pop()
+        display.textContent = result;
     }
-  }
-}) 
-//-------------------------------------------------------------
- 
-operatorsCont.addEventListener("click", (e) => {
-  const displayedNumber = display.textContent;
-  const element = e.target.textContent;
-  const previousVal = display.textContent[display.textContent.length - 1];
-  const oprts = ["+","-","*","/"]
+    
+    if (vEquation[2]) {
+        const result = calculate(vEquation[0], vEquation[1], vEquation[2]);
+        display.textContent = `${result} ${e.target.textContent} `; 
+    }
+}
 
+const handleSpecials = (e) => {
+    const element = e.target.textContent
+        //Makes it not possible to have operator as first value
+    if (display.textContent === "0" && 
+        (
+        element === "←" ||
+        element === "C" ||
+        element === "="
+        )
+       ) return;
 
-  if (display.textContent === "0") { //Makes it not possible to have operator as first value
-    return; 
-  }  
-  
-  if (element === "+" || element === "-" || element === "*" || element === "/") {
-    display.textContent = displayedNumber + element;  
-  } 
-  
-  // Prevent operators from being stacked together.
-  if (oprts.includes(previousVal)) { 
-    display.textContent = display.textContent.slice(0, display.textContent.length - 1);  
-  }
- 
-  // Makes it when you click oprt and have already two numbers, it evaluates them.
-  if (element!== "AC") {  
-    if (hasRepeats(display.textContent) || !hasRepeats(display.textContent)) {
-      const result = Math.round(eval(display.textContent.slice(0,-1)) * 100) / 100;     
-      display.textContent = result + element;                 
-    }   
-  } 
-  // Checks if their is any repeating values in the display.
-  function hasRepeats (str) { 
-    return /(.).*\1/.test(str);
-  }
-})
-document.getElementById("negative").addEventListener("click", () => {
-  if (display.textContent === "0") {
-    display.textContent = "-"
-  }
-  else {
-    display.textContent = display.textContent + "-"
-  }
-})
+    switch(element) {
+      case "+/-":
+        negativeInput()
+        break;
+      case "C":
+        clear()
+        break;
+      case "←":
+        display.textContent = String(backspace()).replaceAll(",", " ")
+        break;
+      case ".":
+        firstDecimal()       
+        break; 
+      case "=":
+        if (checkEquation()) return;
+        const equation = display.textContent.split(" ");
+        const result = calculate(equation[0], equation[1], equation[2]);
+        display.textContent = result; 
+        break;
+    }
+}
 
-historyBtn.addEventListener("click", () => {
-  history.classList.toggle("active")
-})
-document.getElementById("clear").addEventListener("click", () => {
-  display.textContent = "0"
-}) 
+const checkEquation = () => {
+    if (!display.textContent.split(" ")[1] || !display.textContent.split(" ")[2]) {
+        alert("Input your equation!")
+        return true;
+    }
+}
+
+const calculate = (n1, oprt, n2) => {
+    let result = ""; 
+    switch(oprt) {  
+       case "+":
+           result = parseFloat(n1) + parseFloat(n2)
+       break;
+       case "-": 
+           result = parseFloat(n1) - parseFloat(n2)
+        break; 
+       case "*":
+          result = parseFloat(n1) * parseFloat(n2)
+       break;
+       case "%":
+           const decimal = parseFloat(n1) / parseFloat(n2)
+           result = decimal.toFixed(6); 
+       break;
+    }  
+    return result;
+}
+
+const clear = () => {
+    display.textContent = "0";
+}
   
-document.getElementById("backspace").addEventListener("click", () => {
-   display.textContent = display.textContent.slice(0, -1) 
-   display.textContent = display.textContent.length <1 ? "0" : display.textContent
-})
-document.getElementById("equal").addEventListener("click", () => {
-  if (display.textContent === "0") {
+const backspace = () => {
+  let result = display.textContent.slice(0, -1);
+  if (display.textContent.length === 1) {
+    return result = "0"
+  }
+  return result;
+}
+
+const firstDecimal = () => { 
+    if (display.textContent === "0") {
+        display.textContent = display.textContent + "."
+    }
+    if (
+        !display.textContent.split(" ")[0].includes(".") ||
+        !display.textContent.split(" ")[2].includes(".")
+        ) {
+        display.textContent = display.textContent + "."
+    }
     return;
-  }
-  else {
-    try {
-      const result = eval(display.textContent)
-      if (result === Infinity) {
-        display.textContent = "No.....";
-        return;
-      }
-      histDisplay.innerHTML += `<li>${display.textContent} = ${result}</li>`
-      display.textContent = result.toFixed(2);
+}
+const negativeInput = () => { 
+    if (display.textContent === "0") {
+        display.textContent = "-";
     }
-    catch {
-      display.textContent = "Error!!"
+    if (display.textContent.split(" ")[1]) {
+        display.textContent = display.textContent + "-"
     }
-  }
+}
+
+operators.forEach(oprt => {
+  oprt.addEventListener("click", addOprts)
+})
+specialBtns.forEach(btn => {
+  btn.addEventListener("click", handleSpecials)
+})
+integers.forEach(numb => {
+  numb.addEventListener("click", handleIntegers)
 })
 
-document.querySelector("#erase").addEventListener("click", () => {
-  histDisplay.replaceChildren();
-})
+window.addEventListener("keydown", (e) => {
+    const eq = display.textContent.split(" ")
+    if (e.key >= 0 && e.key <= 9) {
+        if (display.textContent === "0") display.textContent = e.key
+        else display.textContent += e.key;
+    }
+    else if (e.key === "Backspace") {
+        display.textContent = display.textContent.slice(0, -1);
+    }
+    else if (e.key === " ") display.textContent += " "
+    else if (
+        e.key === "+" || e.key === "-" || e.key === "*" || e.key === "%"
+        ) display.textContent += e.key;
 
+    if (e.key === "Enter") display.textContent = calculate(eq[0], eq[1], eq[2])
+});
